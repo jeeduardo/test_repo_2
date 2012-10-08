ca82e5d8819db5e49d55bcb4b851eb459ad778c1
Josephson Eduardo <josephson@cascadeo.com>
2012-10-08 13:27:12 +0000

    Committing 'utils' directory and changes to QuickBooks scraping/dump config

--- freshbooks/freshbooks-dump.sh
+++ freshbooks/freshbooks-dump.sh
@@ -2,10 +2,11 @@
 # "crontab -e" entry
 # 0 2 * * * /home/ubuntu/qboe/test_repo_2/freshbooks/freshbooks-dump.sh
 clear
-# 03Oct2012 - testing
-CURR_DAY=$(date +%d)
-CAL_DAY=$(echo `cal` | awk '{print $NF}')
-CURR_MONTH=$(date +%Y-%m)
+# 08Oct2012 - archiving
+CURR_MONTH=$(date +"%Y-%m")
+declare -i IS_MONTHEND
+IS_MONTHEND=$(python /home/ubuntu/qboe/test_repo_2/utils/check_monthend.py)
+# 08Oct2012
 echo "Starting freshbooks dump script"
 # check if vncserver on :64 is running
 IS_VNC_RUNNING=$(ps aux | grep "Xtightvnc" | grep -v "grep")
@@ -19,7 +20,7 @@ fi
 
 export DISPLAY=:64 && cd /home/ubuntu/qboe/test_repo_2/freshbooks && ./freshbooks-dump.py
 # 03Oct2012 - archive files
-if [ $CURR_DAY -eq $CAL_DAY ]; then
+if [ $IS_MONTHEND -eq 1 ]; then
   echo "Archiving CSV backups for this month..."
   tar czvf freshbooks_dump_$CURR_MONTH.tar.gz QB-FreshbooksCascadeoStaging_$CURR_MONTH-*
   rm -rf QB-FreshbooksCascadeoStaging_$CURR_MONTH-*/
